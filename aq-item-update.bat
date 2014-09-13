7c1de4576acba13f975c307c1c1c743eb99113fc
Tommy Goode <tommy@irdequipment.com>
2014-09-12 22:33:30 -0400

    Adding the ability to read username and password from provided command line arguments.

--- aq-item-update.bat
+++ aq-item-update.bat
@@ -2,11 +2,24 @@
 REM - File: aq-item-update.bat
 REM - Description: Runs a SQL script that updates 5Star item info from AQ database
 REM - Author: Devin Spikowski
+REM - Modified by: Tommy Goode ;)
+
+IF %1.==. GOTO ArgError
+IF %2.==. GOTO ArgError
+
+:SQL
 echo Executing script, do NOT close this window until complete.
 echo ======================================================
-sqlcmd -U [username] -P [password] -S IRD2K12\SQL2012 -d AQEXPORT -i C:\AQitemUpdate\aq-item-update.sql -o C:\AQitemUpdate\results.txt
+sqlcmd -U %1 -P %2 -S IRD2K12\SQL2012 -d AQEXPORT -i C:\AQitemUpdate\aq-item-update.sql -o C:\AQitemUpdate\results.txt
 echo ======================================================
 echo.
 echo Script complete. Check C:\AQitemUpdate\results.txt for details.
 echo.
 PAUSE
+GOTO End
+
+:ArgError
+echo Please provide username and password for the SQL DB.
+echo Usage: aq-item-update.bat [username] [password]
+
+:End
