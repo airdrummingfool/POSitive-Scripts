a2358b0dffa05e093a4d65554c02b700e9cfeb14
Tommy Goode <tommy@irdequipment.com>
2014-09-12 22:52:40 -0400

    Added time reporting.

--- aq-item-update.bat
+++ aq-item-update.bat
@@ -8,10 +8,13 @@ IF %1.==. GOTO ArgError
 IF %2.==. GOTO ArgError
 
 :SQL
+echo.
+echo Start time: %DATE% %TIME%
 echo Executing script, do NOT close this window until complete.
 echo ======================================================
 sqlcmd -U %1 -P %2 -S IRD2K12\SQL2012 -d AQEXPORT -i %~dp0aq-item-update.sql -o %~dp0results.txt
 echo ======================================================
+echo End time: %DATE% %TIME%
 echo.
 echo Script complete. Check %~dp0results.txt for details.
 echo.
@@ -21,5 +24,6 @@ GOTO End
 :ArgError
 echo Please provide username and password for the SQL DB.
 echo Usage: aq-item-update.bat [username] [password]
+PAUSE
 
 :End
