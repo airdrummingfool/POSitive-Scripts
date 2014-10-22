f2f7674c5083b18c323a0659fa07e45a0e8649ea
Tommy Goode <tommy@irdequipment.com>
2014-10-22 01:54:05 -0400

    Improving AQ SKU change script, and moving one of the embedded sql statements to its own file.

---
+++ aq-sku/invno-to-vendorid.sql
@@ -0,0 +1,8 @@
+SET NOCOUNT ON;
+DECLARE @INVNO int;
+-- SET @INVNO = 2001922; -- Used when testing, otherwise value should be passed in to next line
+SET @INVNO = $(INVNO);
+
+SELECT VED_FORSKU
+  FROM ITPrice JOIN VENDetail  ON ITP_PVendorID = VED_V_ID
+  WHERE ITP_INVNO = @INVNO
--- update-aq-sku.bat
+++ update-aq-sku.bat
@@ -6,6 +6,7 @@ REM - Author: Tommy Goode
 set DB=IRD_MASTER
 
 : Enter Primary SKU of item
+set PrimarySKU=null
 set /p PrimarySKU="Enter Primary SKU of item: "
 echo.
 
@@ -13,7 +14,7 @@ set INVNO=null
 
 for /f "delims=" %%a in ('call sqlcmdwrapper.bat -d %DB% -Q "SET NOCOUNT ON; SELECT TOP 1 BAR_INVNO FROM BARCODES WHERE BAR_ID = 1 AND BAR_BARCODE='%PrimarySKU%';" -h -1 -W') do @set INVNO=%%a
 
-IF "%INVNO%" == "null" (echo Couldn't find item! && exit /b)
+IF "%INVNO%" == "null" (echo Couldn't find item! && PAUSE && exit /b)
 
 set DESC=null
 
@@ -21,18 +22,26 @@ for /f "delims=" %%a in ('call sqlcmdwrapper.bat -d %DB% -Q "SET NOCOUNT ON; SEL
 
 set CurrentAQSKU=null
 
-for /f "delims=" %%a in ('call sqlcmdwrapper.bat -d %DB% -Q "SET NOCOUNT ON; SELECT TOP 1 CASE WHEN BAR_BARCODE='' THEN 'empty' ELSE BAR_BARCODE END FROM BARCODES WHERE BAR_ID = 2 AND BAR_INVNO='%INVNO%';" -h -1 -W') do @set CurrentAQSKU=%%a
+for /f "delims=" %%a in ('call sqlcmdwrapper.bat -d %DB% -i aq-sku\invno-to-aqsku.sql -h -1 -W') do @set CurrentAQSKU=%%a
 IF "%CurrentAQSKU%" == "null" (set CurrentAQSKU=empty)
+
+set VendorID=null
+for /f "usebackq delims=" %%a in (`call sqlcmdwrapper.bat -d %DB% -i aq-sku\invno-to-vendorid.sql -h -1 -W`) do @set VendorID=%%a
+IF "%VendorID%" == "null" (set VendorID=empty)
 
 echo You are about to update %PrimarySKU%: %DESC% (INVNO is %INVNO%)
 echo Current AQ SKU is %CurrentAQSKU%
-set /p NewAQSKU="Enter new AQ SKU: "
+echo Recommended AQ SKU is %PrimarySKU%@%VendorID%
+set NewAQSKU=null
+set /p NewAQSKU="Enter new AQ SKU (leave empty to use recommended): "
+
+IF "%NewAQSKU%" == "null" (echo Using recommended... && set NewAQSKU=%PrimarySKU%@%VendorID%)
 
 call sqlcmdwrapper.bat -d %DB% -i aq-sku\update-aq-sku.sql -h -1 -W -v INVNO="%INVNO%" NewAQSKU="%NewAQSKU%"
 
 set NewAQSKU=null
 
-for /f "delims=" %%a in ('call sqlcmdwrapper.bat -d %DB% -Q "SET NOCOUNT ON; SELECT TOP 1 CASE WHEN BAR_BARCODE='' THEN 'empty' ELSE BAR_BARCODE END FROM BARCODES WHERE BAR_ID = 2 AND BAR_INVNO='%INVNO%';" -h -1 -W') do @set NewAQSKU=%%a
+for /f "delims=" %%a in ('call sqlcmdwrapper.bat -d %DB% -i aq-sku\invno-to-aqsku.sql -h -1 -W') do @set NewAQSKU=%%a
 echo New AQ SKU is %NewAQSKU%
 
 echo.
