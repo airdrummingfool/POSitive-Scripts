b532c42a13a9e65522b92e2fe7f3ce6e9417fc2e
Tommy Goode <tommy@irdequipment.com>
2014-10-22 01:56:05 -0400

    Adding a script to find AQ SKUs that are likely incorrect (don't match VendorSKU@VendorID).

---
+++ aq-sku/fixable-aq-skus.sql
@@ -0,0 +1,16 @@
+-- Comparing Best Guess AQ SKUs to Actual
+
+SELECT ITE_INVNO, ITE_BARCODE AS PRIMARY_SKU, VIN_VINO AS VENDOR_SKU, VED_FORSKU AS VENDOR_ID,
+  RTRIM(VIN_VINO)+'@'+VED_FORSKU AS RECOMMENDED_AQSKU,
+  BAR_BARCODE AS CURRENT_AQSKU, *
+  FROM ITEMS
+    LEFT JOIN BARCODES ON ITE_INVNO = BAR_INVNO AND BAR_ID = 2
+    JOIN ITPrice ON ITE_INVNO = ITP_INVNO
+    JOIN VENDetail ON ITP_PVendorID = VED_V_ID
+    LEFT JOIN VENINV ON ITE_INVNO = VIN_INVNO
+  WHERE VED_FORSKU != ''
+    AND RTRIM(VIN_VINO)+'@'+VED_FORSKU != BAR_BARCODE
+    AND ITE_Weight = 0 -- Weight is only set from AQ, so if it's set it must be matching
+    AND ITE_FreightClass = 0 -- Freight class is only set from AQ, so if it's set it must be matching
+
+ -- From Primary SKU: IIF((CHARINDEX('@', ITE_BARCODE) != 0), LEFT(ITE_BARCODE, CHARINDEX('@', ITE_BARCODE)-1), ITE_BARCODE)
