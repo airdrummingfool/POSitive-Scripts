6d9e7401f4c6fa902e2a40bf50d2998b846eb4de
Tommy Goode <tommy@irdequipment.com>
2014-10-27 21:32:46 -0500

    Reorganizing the repo, pt 2.

--- aq-sku/invno-to-aqsku.sql
+++
@@ -1,3 +0,0 @@
-SET NOCOUNT ON;DECLARE @INVNO int;
--- SET @INVNO = 2001922; -- Used when testing, otherwise value should be passed in to next line
-SET @INVNO = $(INVNO);SELECT TOP 1 CASE WHEN BAR_BARCODE='' THEN 'empty' ELSE BAR_BARCODE END  FROM BARCODES  WHERE BAR_ID = 2 AND BAR_INVNO = @INVNO
