aa00ce7c6598449f00a051593b567bdb0d0df8d0
Tommy Goode <tommy@irdequipment.com>
2014-11-21 21:51:09 -0500

    Adding a script to clone the current production db to the training db.

---
+++ update-training-db.bat
@@ -0,0 +1,24 @@
+@echo off
+REM - File: update-training-db.bat
+REM - Description: Backs up the master db and restores it over the current training db, then calls the station wallpaper update script
+REM - Author: Tommy Goode
+REM - Copyright 2014 International Restaurant Distributors, Inc.
+
+call util\loadconfig.bat
+
+echo About to update %positive_training_db% with the contents of %positive_db%
+echo Please make sure %positive_training_db% is not in use.
+pause
+
+set "tmpdir=%~dp0update-training-db\temp"
+if not exist "%tmpdir%\" mkdir "%tmpdir%"
+
+call util\sqlcmdwrapper.bat -i update-training-db\update-training-db.sql -h -1 -W -v TEMP_DIR="%tmpdir%" MASTER_DB="%positive_db%" TRAINING_DB="%positive_training_db%"
+
+del /Q "%tmpdir%\*"
+echo.
+
+pause
+
+echo.
+call update-station-wallpaper.bat %positive_training_db% "%training_wallpaper%" 0
---
+++ update-training-db/update-training-db.sql
@@ -0,0 +1,16 @@
+/*
+ * Backs up the master db and restores it over the training db
+ *
+ * @author: Tommy Goode
+ * @copyright: 2014 International Restaurant Distributors, Inc.
+ *
+ */
+
+BACKUP DATABASE $(MASTER_DB)
+  TO DISK = '$(TEMP_DIR)\master-to-training-db.bak'
+GO
+
+RESTORE DATABASE $(TRAINING_DB)
+  FROM DISK = '$(TEMP_DIR)\master-to-training-db.bak'
+  WITH REPLACE
+GO
