69f03eca0b3d759b68bb8d629bd8906b6267de7b
Tommy Goode <tommy@irdequipment.com>
2014-12-09 01:08:25 -0500

    make the config.ini path relative to the loadconfig.bat file, so the path of the calling .bat file doesn't matter.

--- util/loadconfig.bat
+++ util/loadconfig.bat
@@ -4,4 +4,4 @@ REM - Description: Load environment variables from a local file
 REM - Author: Tommy Goode
 
 : load username, password, instance, db names from config.ini. Format: key=value
-for /F "tokens=*" %%I in (config.ini) do set %%I
+for /F "tokens=*" %%I in (%~dp0..\config.ini) do set %%I
