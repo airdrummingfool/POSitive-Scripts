4d45abe640efdc43c14aa4b27274870989ea34ef
Tommy Goode <tommy@irdequipment.com>
2015-01-10 18:20:04 -0500

    Adding more detail and ORDER BY statements to the barcode problems sql file.

--- advanced/barcode-problems.sql
+++ advanced/barcode-problems.sql
@@ -31,20 +31,24 @@ SELECT * FROM BARCODES
 -- List all non-matches
 SELECT * FROM BARCODES
   WHERE BAR_ID = 2
+    AND BAR_BARCODE <> ''
     AND NOT EXISTS (
       SELECT AQ_SKU FROM #AQ_SKUs
         WHERE AQ_SKU = rtrim(BAR_BARCODE)
     )
+  ORDER BY BAR_BARCODE
 
 -- List all AQ SKUs with whitespace problems (i.e. would match up to AutoQuotes data if whitespaces were ignored)
-SELECT BAR_BARCODE, AQ_SKU, * FROM BARCODES
+SELECT BAR_BARCODE AS FIVESTAR_AQ_SKU, AQ_SKU AS GOOD_AQ_SKU, *
+  FROM BARCODES
   JOIN #AQ_SKUs ON replace(BAR_BARCODE, ' ', '') = replace(AQ_SKU, ' ', '')
   WHERE BAR_ID = 2
     AND BAR_TYPE = 'I'
     AND AQ_SKU != BAR_BARCODE
     AND AQ_SKU IS NOT NULL
+  -- ORDER BY makes this query super slow
 
--- List all Vendor IDs that are off by whitespace from a valid AQ SKU
+-- List all Vendor IDs that are off by whitespace from a valid AQ SKU (can be fixed automatically)
 SELECT VIN_VINO AS CURRENT_VENDOR_SKU, left(BAR_BARCODE, charindex('@', BAR_BARCODE) - 1) AS CORRECT_VENDOR_SKU, *
   FROM BARCODES
     INNER JOIN ITPrice ON BAR_INVNO = ITP_INVNO
@@ -57,8 +61,9 @@ SELECT VIN_VINO AS CURRENT_VENDOR_SKU, left(BAR_BARCODE, charindex('@', BAR_BARC
     AND VIN_VINO != left(BAR_BARCODE, charindex('@', BAR_BARCODE) - 1)
     AND VIN_VINO = replace(left(BAR_BARCODE, charindex('@', BAR_BARCODE) - 1), ' ', '')
     AND EXISTS (SELECT * FROM #AQ_SKUs WHERE rtrim(AQ_SKU) = rtrim(BAR_BARCODE))
+  ORDER BY CORRECT_VENDOR_SKU
 
--- Items where the Vendor SKU doesn't match the Vendor SKU derived from a correct AQ SKU
+-- Items where the Vendor SKU doesn't match the Vendor SKU derived from a correct AQ SKU (must be fixed manually)
 SELECT VIN_VINO AS CURRENT_VENDOR_SKU, left(BAR_BARCODE, charindex('@', BAR_BARCODE) - 1) AS AQ_VENDOR_SKU, *
   FROM BARCODES
     INNER JOIN ITPrice ON BAR_INVNO = ITP_INVNO
@@ -69,4 +74,6 @@ SELECT VIN_VINO AS CURRENT_VENDOR_SKU, left(BAR_BARCODE, charindex('@', BAR_BARC
     AND ITP_PVendorID = VIN_V_ID
     AND charindex('@', BAR_BARCODE) > 0
     AND VIN_VINO != left(BAR_BARCODE, charindex('@', BAR_BARCODE) - 1)
+    AND VIN_VINO != replace(left(BAR_BARCODE, charindex('@', BAR_BARCODE) - 1), ' ', '')
     AND EXISTS (SELECT * FROM #AQ_SKUs WHERE rtrim(AQ_SKU) = rtrim(BAR_BARCODE))
+  ORDER BY AQ_VENDOR_SKU
