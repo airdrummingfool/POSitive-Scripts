22539e184ccffac184a2a783015f4c0a0d605f55
Tommy Goode <tommy@irdequipment.com>
2015-08-18 19:03:41 -0400

    Adding Site to the AQ5StarLink table and making sure there is an empty web/extended notes field for every AQ-matched item.

--- aq-item-update/aq-item-update.sql
+++ aq-item-update/aq-item-update.sql
@@ -8,21 +8,33 @@
  */
 
 -- Create a temporary table to link POSitive 5 Star inventory numbers to AQ product IDs
-use [$(autoquotes_db)]
+USE [$(autoquotes_db)]
 IF OBJECT_ID('dbo.AQ5StarLink', 'U') IS NOT NULL DROP TABLE dbo.AQ5StarLink
-select productid, bar_invno as invno into AQ5StarLink
-from Products inner join [$(positive_db)].dbo.barcodes
-  on rtrim(bar_barcode) = left(rtrim(model) + '@' + rtrim(vendornumber),20) and bar_id = 2
-go
+SELECT productid, BAR_INVNO AS invno, BAR_Site AS site
+INTO AQ5StarLink
+FROM Products INNER JOIN [$(positive_db)].dbo.barcodes
+  ON RTRIM(BAR_BARCODE) = LEFT(RTRIM(model) + '@' + RTRIM(vendornumber), 20) AND BAR_ID = 2
+GO
 
 -- Begin updating the POSitive 5 Star items
-use [$(positive_db)]
+USE [$(positive_db)]
 
--- First, make sure we've increased UDF size limits (for storing CutsheetLink - this works as long as you don't directly edit the CutsheetLink field in POSitive)
+-- Create empty Notes fields for all items that don't have one but have AQ data
+DECLARE @max_notes_pid AS int = (SELECT max(NTS_PrimaryID) FROM NOTES)
+INSERT INTO NOTES
+  SELECT ' ' AS NTS_NOTE, invno as NTS_N_ID, 0 as NTS_ODTE, 0 AS NTS_OTIM, 0 AS NTS_ETIM, '' AS NTS_TICK, '' AS NTS_DESC, 'X' AS NTS_TYPE, '' AS NTS_T_ID, '' AS NTS_CUSTID, '' AS NTS_STATUS, 0 AS NTS_AttachmentType, 0 as NTS_AttachmentID, '' AS NTS_AttachmentFileName, site+'IRD@'+left(REPLACE(newid(), '-', ''), 8) AS NTS_GUID, site as NTS_Site, @max_notes_pid + ROW_NUMBER() over(ORDER BY (SELECT NULL)) AS NTS_PrimaryID, 0 AS NTS_Revision, 0 AS NTS_Invisible
+    FROM [$(autoquotes_db)].dbo.AQ5StarLink WHERE invno NOT IN (
+      SELECT NTS_N_ID FROM NOTES WHERE NTS_TYPE = 'X'
+    )
+GO
+
+-- Make sure we've increased UDF size limits (for storing CutsheetLink - this works as long as you don't directly edit the CutsheetLink field in POSitive)
 IF (
-  (SELECT CHARACTER_MAXIMUM_LENGTH FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'UDFIELDS' AND COLUMN_NAME = 'UDF_UDFLD1') < 60)
+  SELECT CHARACTER_MAXIMUM_LENGTH FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'UDFIELDS' AND COLUMN_NAME = 'UDF_UDFLD1'
+  ) < 60
   ALTER TABLE UDFIELDS ALTER COLUMN UDF_UDFLD1 char(60)
-go
+GO
 
 -- Price Level 1 = List Price
 update itprice set itp_price1 = ListPrice
