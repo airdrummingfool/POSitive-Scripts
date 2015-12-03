9cd5d9b28d003a1787e9b0bf3153092278011e75
Tommy Goode <tommy@irdequipment.com>
2015-12-03 11:06:24 -0600

    Adding an AR script to move an applied payment from one charge to another.

---
+++ ar-adjustment/move-applied-payment.bat
@@ -0,0 +1,43 @@
+@echo off
+REM - File: move-applied-payment.bat
+REM - Description: Makes it easy to move an applied payment to another charge in 5 Star.
+REM - Author: Tommy Goode
+
+: Setup
+: call util\loadconfig.bat
+
+: Input
+set PaymentTNum=
+set /P PaymentTNum=Enter the Payment ID:
+set OldChargeTNum=
+set /P OldChargeTNum=Enter the Charge ID the payment is currently applied to:
+set NewChargeTNum=
+set /P NewChargeTNum=Enter the Charge ID the payment should be moved to:
+
+: Confirmation
+set CusCode=null
+for /f "delims=" %%a in ('"call util\sqlcmdwrapper.bat -d %DB% -i %~dp0tnum-to-cuscode.sql -h -1 -W -v TNum=%PaymentTNum%"') do @set CusCode=%%a
+
+set PaymentBalance=null
+for /f "delims=" %%a in ('"call util\sqlcmdwrapper.bat -d %DB% -i %~dp0tnum-to-balance.sql -h -1 -W -v TNum=%PaymentTNum%"') do @set PaymentBalance=%%a
+
+set OldChargeBalance=null
+for /f "delims=" %%a in ('"call util\sqlcmdwrapper.bat -d %DB% -i %~dp0tnum-to-balance.sql -h -1 -W -v TNum=%OldChargeTNum%"') do @set OldChargeBalance=%%a
+
+set NewChargeBalance=null
+for /f "delims=" %%a in ('"call util\sqlcmdwrapper.bat -d %DB% -i %~dp0tnum-to-balance.sql -h -1 -W -v TNum=%NewChargeTNum%"') do @set NewChargeBalance=%%a
+
+echo "You want to move the application of %CusCode%'s %PaymentTNum%, which has a balance of $%PaymentBalance%, from %OldChargeTNum% (balance %OldChargeBalance%) to %NewChargeTNum% (balance %NewChargeBalance%). Is that correct?"
+set /P Confirmation=(Y/N)
+
+if /I NOT '%Confirmation%'=='Y' goto Input
+
+: SQL
+call util\sqlcmdwrapper.bat -d %DB% -i %~dp0move-applied-payment.sql -h -1 -W 2>&1
+
+: Conclusion
+set NewChargeNewBalance=null
+for /f "delims=" %%a in ('"call util\sqlcmdwrapper.bat -d %DB% -i %~dp0tnum-to-balance.sql -h -1 -W -v TNum=%NewChargeTNum%"') do @set NewChargeNewBalance=%%a
+echo New balance for charge %NewChargeTNum% is $%NewChargeNewBalance%.
+
+pause
---
+++ ar-adjustment/move-applied-payment.sql
@@ -0,0 +1,41 @@
+/*
+ * Moves an applied payment amount from one charge to another
+ *
+ * @author: Devin Spikowski
+ * @modified-by: Tommy Goode
+ * @copyright: 2015 International Restaurant Distributors, Inc.
+ *
+ */
+
+DECLARE @PaymentTNum as int = $(PaymentTNum)
+DECLARE @OldChargeTNum as int = $(OldChargeTNum)
+DECLARE @NewChargeTNum as int = $(NewChargeTNum)
+
+UPDATE AR_CRF
+  SET CRF_TNM2 = @NewChargeTNum, CRF_TID2 = (SELECT ART_T_ID FROM AR_TRN WHERE ART_TNUM = @NewChargeTNum)
+  WHERE CRF_TNM2 = @OldChargeTNum AND CRF_TNM1 = @PaymentTNum
+
+DECLARE @oldChargeTotalPaid as decimal(13,4) = (SELECT sum(CRF_AMNT) FROM AR_CRF WHERE CRF_TNM2 = @OldChargeTNum)
+DECLARE @newChargeTotalPaid as decimal(13,4) = (SELECT sum(CRF_AMNT) FROM AR_CRF WHERE CRF_TNM2 = @NewChargeTNum)
+
+UPDATE AR_TRN
+  SET
+    ART_OPCL =
+      CASE ART_TTOT - @oldChargeTotalPaid
+        WHEN 0 THEN 'C'
+        ELSE 'O'
+      END,
+    ART_BALA = ART_TTOT - @oldChargeTotalPaid,
+    ART_PAID = @oldChargeTotalPaid
+  WHERE ART_TNUM = @OldChargeTNum
+
+UPDATE AR_TRN
+  SET
+    ART_OPCL =
+      CASE ART_TTOT - @newChargeTotalPaid
+        WHEN 0 THEN 'C'
+        ELSE 'O'
+      END,
+    ART_BALA = ART_TTOT - @newChargeTotalPaid,
+    ART_PAID = @newChargeTotalPaid
+  WHERE ART_TNUM = @NewChargeTNum
